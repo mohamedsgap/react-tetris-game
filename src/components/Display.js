8c776715424bd3b1cb69ac309ff6cf194276ce42
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2020-02-25 12:12:02 +0200

    Add Display component

---
+++ src/components/Display.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+
+const Display = ({gameOver, text}) => {
+    return (
+    <div>
+        <h3>Display of the game</h3>
+    </div>
+    )
+}
+
+export  default Display;
