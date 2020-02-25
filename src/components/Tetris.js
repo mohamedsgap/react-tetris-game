d8a65a50f5aca0c181aa76a869b740cc561bc461
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2020-02-25 11:48:05 +0200

    Add Tetris component

---
+++ src/components/Tetris.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+
+const Tetris = () => {
+    return (
+    <div>
+        <h1>Tetris game is in developing process!!!</h1>
+    </div>
+    )
+}
+
+export  default Tetris;
