d03d4eff567d53920d6ed92f7c4bc6b40b2a47c6
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2020-02-25 12:14:45 +0200

    Update Tetris component

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -1,11 +1,23 @@
 import React from 'react'
-
+import Display from './Display'
+import Stage from './Stage'
+import StartBtn from './StartBtn'
 
 
 const Tetris = () => {
+
     return (
     <div>
         <h1>Tetris game is in developing process!!!</h1>
+        <Stage />
+        <aside>
+            <div>
+                <Display text="Score"/>
+                <Display text="Rows"/>
+                <Display text="Levels"/>
+            </div>
+            <StartBtn />
+        </aside>
     </div>
     )
 }
