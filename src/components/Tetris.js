45e19d440a345cbeaf56dc7fc17973c5cf16ef08
Mohamed Abdel Nasser <mohdabdellnasser1@gmail.com>
2020-03-11 08:28:51 +0200

    Update Tetris with Hooks

--- src/components/Tetris.js
+++ src/components/Tetris.js
@@ -1,21 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Display from './Display'
 import Stage from './Stage'
 import StartBtn from './StartBtn'
-import { buildStage } from '../utils/gameHelpers'
 import { TetrisWrapperStyling, TetrisStyling } from './TetrisStyling';
+import { useStage } from '../game-hooks/useStage'
+import { usePlayer } from '../game-hooks/usePlayer'
+
 const Tetris = () => {
+  const [dropTime, setDropTime] = useState(null)
+  const [gameOver, setGameOver] = useState(false)
+  const [player] = usePlayer()
+  //const [stage, setStage] = useStage(player)
+  const [stage, setStage] = useStage(player)
+
   return (
     <TetrisWrapperStyling>
      <h1 className="gameTitle"><span role="img" aria-label="tetris-emoji">🧩</span>React Tetris Game<span role="img" aria-label="tetris-emoji">🧩</span></h1>
       <TetrisStyling>
-        <Stage stage={buildStage()} />
+        <Stage stage={stage} />
         <aside>
+          { gameOver ? (
+            <Display gameOver={gameOver} text="Game Over" />
+
+          ): (
           <div>
             <Display text="Score" />
             <Display text="Rows" />
             <Display text="Level" />
           </div>
+          )}
           <StartBtn />
         </aside>
       </TetrisStyling>
